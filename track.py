67667989b066cf0f3ddd4800e7604a3c7a35b52c
Richard Liaw <rliaw@berkeley.edu>
2018-06-20 17:06:15 -0700

    Initial changes for structure

---
+++ track.py
@@ -0,0 +1,8 @@
+# Analytical Tools
+def get_trials(project_id):
+    pass
+
+def get_trial_results(trial_id):
+    pass
+
+
---
+++ trial.py
@@ -0,0 +1,15 @@
+class Trial(object):
+    def __init__(self, project_name, params_dict):
+        pass
+
+    def start(self):
+        pass
+
+    def log_metric(self, **kwargs):
+        pass
+
+    def log_data(self, data, **kwargs):
+        pass
+
+    def close(self):
+        pass
